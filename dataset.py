90a08b1b777cea3fd4cfdf91ea9308ca15c6ae0f
b._.obo <76933244+boookk@users.noreply.github.com>
2021-12-05 18:02:59 +0900

    Update dataset.py

--- dataset.py
+++ dataset.py
@@ -1,51 +1,96 @@
-import torch
-from torch.utils.data import Dataset
-from torchvision import transforms
-
-from PIL import Image
+import os
 from pathlib import Path
-from itertools import chain
+
+import torch
+import cv2
+import numpy as np
+from torch.utils.data import Dataset
+
+import skimage.io as io
+from skimage.transform import resize
 
 
 class AbnormalDataset(Dataset):
-    def __init__(self, path, frame_size, size):
-        self.path = Path(path).absolute()
-        self.frame_size = frame_size
-        self.size = size
-        self.class_path = list(self.path.iterdir())
-        self.data_ = list(chain(*[c.iterdir() for c in self.class_path]))
-        self.data = [d for d in self.data_ if len(list(d.glob('**/*.png'))) > self.frame_size]
-        self.spatial_transforms = self.get_spatial_transforms()
-        self.classes = ['normal', 'swoon']
 
-    def get_temporal_transforms(self, s):
-        middle = s // 2
-        half = self.frame_size // 2
-        start = middle - half
-        end = middle + half
-        return start, end
+    def __init__(self, data_path, split='train', clip_len=16):
+        self.path = Path(data_path)
+        self.clip_len = clip_len
+        self.split = split
 
-    def get_spatial_transforms(self):
-        transform = transforms.Compose([
-            transforms.Resize((self.size, self.size)),
-            transforms.ToTensor(),
-            transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
-        ])
+        self.fnames, labels = [], []
+        for dir_path in sorted(self.path.iterdir()):
+            for file_path in dir_path.iterdir():
+                self.fnames.append(file_path)
+                labels.append(dir_path.name)
 
-        return transform
+        assert len(labels) == len(self.fnames)
+        print('Number of {} videos: {:d}'.format(split, len(self.fnames)))
 
-    def __getitem__(self, idx):
-        target = self.classes.index(str(self.data[idx].parent).split('/')[-1])
-        img_list = list(self.data[idx].glob('**/*.png'))
-        start, end = self.get_temporal_transforms(len(img_list))
+        self.label2index = {label: index for index, label in enumerate(sorted(set(labels)))}
+        self.label_array = np.array([self.label2index[label] for label in labels], dtype=int)
 
-        clip = []
-        for img_path in img_list[start:end]:
-            img = Image.open(img_path).convert('RGB')
-            img = self.spatial_transforms(img)
-            clip.append(img)
-        clip = torch.stack(clip, 0).permute(1, 0, 2, 3)
-        return clip, target
+        if not os.path.exists('labels.txt'):
+            with open('labels.txt', 'w') as f:
+                for id, label in enumerate(sorted(self.label2index)):
+                    f.writelines(str(id + 1) + ' ' + label + '\n')
 
     def __len__(self):
-        return len(self.data)
+        return len(self.fnames)
+
+    def __getitem__(self, index):
+        buffer = self.load_frames(self.fnames[index])
+        labels = np.array(self.label_array[index])
+
+        if self.split == 'test':
+            buffer = self.randomflip(buffer)
+
+        buffer = self.normalize(buffer)
+        buffer = self.to_tensor(buffer)
+
+        return torch.from_numpy(buffer), torch.from_numpy(labels)
+
+    def randomflip(self, buffer):
+        if np.random.random() < 0.5:
+            for i, frame in enumerate(buffer):
+                frame = cv2.flip(buffer[i], flipCode=1)
+                buffer[i] = cv2.flip(frame, flipCode=1)
+
+        return buffer
+
+    def normalize(self, buffer):
+        for i, frame in enumerate(buffer):
+            frame -= np.array([[[90.0, 98.0, 102.0]]])
+            buffer[i] = frame
+
+        return buffer
+
+    def to_tensor(self, buffer):
+        return buffer.transpose((3, 0, 1, 2))
+
+    def load_frames(self, file_dir):
+        clip = sorted(list(Path(file_dir).glob('**/*.jpg')))
+        time_index = np.random.randint(len(clip) - self.clip_len)
+        clip = clip[time_index:time_index + self.clip_len]
+
+        clip = np.array([resize(io.imread(str(frame)), output_shape=(112, 200), preserve_range=True).astype(np.float64) for frame in clip]).astype(np.float32)
+
+        # center crop
+        clip = clip[:, :, 44:44 + 112, :]
+
+        return clip
+
+
+if __name__ == "__main__":
+    from torch.utils.data import DataLoader
+    train_data = AbnormalDataset(data_path='/home/bobo/output/detection/train', split='train', clip_len=16)
+    test_data = AbnormalDataset(data_path='/home/bobo/output/detection/test', split='test', clip_len=16)
+    train_loader = DataLoader(train_data, batch_size=32, shuffle=True, num_workers=0)
+
+    for i, sample in enumerate(train_loader):
+        inputs = sample[0]
+        labels = sample[1]
+        print(inputs.size())
+        print(labels)
+
+        if i == 1:
+            break
